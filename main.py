85f33dce2ccd0ba09a05fd4306eddbe0c87d3608
lolguy66 <alexanderflotti@gmail.com>
2020-02-20 09:32:04 +0000

    AIT WE DID IT

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ POSTS = {
 }
 
 env = Environment(loader=PackageLoader('main', 'templates'))
-home_template = env.get_template('home.html')
+home_template = env.get_template('frontpage.html')
 post_template = env.get_template('post.html')
 
 posts_metadata = [POSTS[post].metadata for post in POSTS]
 tags = [post['tags'] for post in posts_metadata]
 home_html = home_template.render(posts=posts_metadata, tags=tags)
 
-with open('../recipes/index.html', 'w') as file:
+with open('../x-out/index.html', 'w') as file:
     file.write(home_html)
 
 for post in POSTS:
@@ -39,57 +39,8 @@ for post in POSTS:
     }
 
     post_html = post_template.render(post=post_data)
-    post_file_path = '../recipes/posts/{slug}.html'.format(slug=post_metadata['slug'])
+    post_file_path = '../x-out/posts/{slug}.html'.format(slug=post_metadata['slug'])
 
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
         file.write(post_html)
-
-
-"""
-import os
-from datetime import datetime
-from jinja2 import Environment, PackageLoader
-from markdown2 import markdown
-
-
-POSTS = {}
-
-for markdown_post in os.listdir('content'):
-    file_path = os.path.join('content', markdown_post)
-
-    with open(file_path, 'r') as file:
-        POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
-
-    
-POSTS = {
-    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
-}
-# skilgreina template sem á að nota
-env = Environment(loader=PackageLoader('main', 'templates'))
-home_template = env.get_template('home.html')
-post_template = env.get_template('post.html')
-
-posts_metadata = [POSTS[post].metadata for post in POSTS]
-tags = [post['tags'] for post in posts_metadata]
-home_html = home_template.render(posts=posts_metadata, tags=tags)
-# ekki output heldur upp úr vinnumöppuni og á nýja
-with open('../recipes/posts/index.html', 'w') as file:
-    file.write(home_html)
-
-for post in POSTS:
-    post_metadata = POSTS[post].metadata
-
-    post_data = {
-        'content': POSTS[post],
-        'title': post_metadata['title'],
-        'date': post_metadata['date']
-    }
-
-    post_html = post_template.render(post=post_data)
-    post_file_path = '../recipes2/posts/{slug}.html'.format(slug=post_metadata['slug'])
-
-    os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
-    with open(post_file_path, 'w') as file:
-        file.write(post_html)
-"""
